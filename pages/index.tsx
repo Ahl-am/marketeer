b837816b490e048f2666ba4d3fae2ae6146a2799
Ahl-am <ahlam.moh.ibrahim@gmail.com>
2023-09-24 02:35:20 +0300

    adjust styling+add character limit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 export default function Home() {
   return (
-    <main className="bg-primary min-h-[100vh] h-full">
+    <div className="bg-primary min-h-[100vh] h-full pt-[20vh]">
       <h1 className="text-secondary text-[30px] sm:text-[40px] lg:text-[55px] text-center pt-5 font-montserrat font-semibold">
         Marketeer
       </h1>
-      <h2 className="text-white  text-[18px] sm:text-[24px] lg-text[30px] text-center py-2">
+      <h2 className="text-white  text-[18px] sm:text-[24px] lg-text[30px] text-center py-2 mb-6">
         Your Marketing Plan Architect
       </h2>
-      <div className="max-w-7xl flex flex-col items-center justify-center px-2 sm:px-4 lg:px-6 mx-auto">
-        <input
-          type="text"
-          className="block text-white border border-white rounded-lg px-3 py-2 mx-auto w-full  bg-transparent focus:outline-none"
+      <form className="max-w-5xl flex flex-col items-center justify-center px-2 sm:px-4 lg:px-6 mx-auto gap-5">
+        <div className="relative w-full">
+        <textarea
+          rows={5}
+          className="block text-white border border-white rounded-lg px-3 py-2 mx-auto w-full bg-transparent focus:outline-none resize-none"
           placeholder="Enter your marketing copy here"
         />
-        <button className="rounded-xl px-6 py-2 text-white font-semibold text-[19px] flex justify-center items-center mx-auto bg-secondary my-5">
+        <span className="absolute bottom-2 right-2 text-[14px] font-montserrat">
+          <span></span>/30
+        </span>
+        </div>
+        <button className="rounded-xl px-6 py-2 text-white font-semibold text-[19px] flex justify-center items-center mx-auto bg-secondary">
           Generate
         </button>
-      </div>
-    </main>
+      </form>
+    </div>
   );
 }
